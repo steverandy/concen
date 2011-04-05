d280c80bd968c069a6ba978c09f1cff07243fbf1
Steve Randy Tantra <steve.randy@gmail.com>
2011-04-05 23:27:55 +0800

    Fix session_controller.

--- app/controllers/control_center/admins/sessions_controller.rb
+++ app/controllers/control_center/admins/sessions_controller.rb
@@ -10,18 +10,18 @@ module ControlCenter
       clean_up_passwords(build_resource)
       render_with_scope :new
     end
-
-    # POST /resource/sign_in
+    
     def create
-      resource = warden.authenticate!(:scope => resource_name, :recall => "new")
-      set_flash_message :notice, :signed_in
-      sign_in_and_redirect(resource_name, resource)
+      resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
+      set_flash_message(:notice, :signed_in) if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with resource, :location => redirect_location(resource_name, resource)
     end
-
-    # GET /resource/sign_out
+    
     def destroy
-      set_flash_message :notice, :signed_out if signed_in?(resource_name)
+      signed_in = signed_in?(resource_name)
       sign_out_and_redirect(resource_name)
+      set_flash_message :notice, :signed_out if signed_in
     end
   end
 end
