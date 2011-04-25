e50b83a880b4ce6a47354433ce2750102aec9b5c
Steve Randy Tantra <steve.randy@gmail.com>
2011-04-25 22:18:09 +0800

    Partial view for nested list of pages.

--- app/helpers/control_center/pages_helper.rb
+++ app/helpers/control_center/pages_helper.rb
@@ -1,19 +1,5 @@
 module ControlCenter
   module PagesHelper
-    def nested_pages_for(page)
-      for child in Page.where(:parent_id => page.id)
-        haml_tag :ul, :class => "pages level-#{child.level}" do
-          haml_tag :li do
-            haml_tag :p do
-              haml_tag :span, child.title
-              haml_concat link_to "Edit", edit_control_center_page_path(child)
-              haml_concat link_to "Delete", control_center_page_path(child), :method => :delete, :confirm => "Are you sure?"
-              haml_concat link_to "Add Child", new_control_center_page_path(:level => child.level + 1, :parent_id => child.id)
-            end
-            nested_pages_for child if Page.where(:parent_id => child.id).count > 0
-          end
-        end
-      end
-    end
+
   end
 end
