3e87d5ef6d4cde49f95fcc30aeeb9fb2d51b543f
Steve Randy Tantra <steve.randy@gmail.com>
2011-06-06 22:48:09 +0800

    Cleanup and some commenting.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 require "rack/gridfs"
 
+# Use Rails.application.routes.prepend on Rails 3.1.
 Rails.application.routes.draw do  
   match "/visits/record.gif" => "control_center/visits#record", :as => "record_visit"
   match "/visits/js" => "control_center/visits#visit_recorder_js", :as => "visit_recorder_js"
   mount Rack::GridFS::Endpoint.new(:db => Mongoid.database, :lookup => :path), :at => "gridfs"
-  # match '/gridfs/*path' => 'control_center/gridfs#serve', :as => "gridfs" unless Rails.env == 'production'
   
   scope :constraints => {:subdomain => "controlcenter"}, :module => "control_center", :as => "control_center"  do
     match "/statistics" => "main#statistics", :as => "statistics"
--- lib/control_center/engine.rb
+++ lib/control_center/engine.rb
@@ -1,7 +1,9 @@
 require "rails"
 
 module ControlCenter  
-  class Engine < Rails::Engine    
+  class Engine < Rails::Engine
+    # Use Rails.application.routes.prepend on Rails 3.1
+    # and disasble the following code block.
     initializer :prepend_routing_paths do |app|
       paths.config.routes.to_a.each do |route|
         app.routes_reloader.paths.unshift(route) if File.exists?(route)
