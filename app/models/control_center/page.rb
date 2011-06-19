5c72d5f255042b33361d22ba82ab9f9d6579fdf9
Steve Randy Tantra <steve.randy@gmail.com>
2011-06-19 23:31:22 +0800

    parse_raw_text should be performed if the record is new.

--- app/models/control_center/page.rb
+++ app/models/control_center/page.rb
@@ -6,7 +6,7 @@ module ControlCenter
     include Mongoid::Document
     include Mongoid::Timestamps
 
-    references_many :children, :class_name => "ControlCenter::Page",:foreign_key => :parent_id, :inverse_of => :parent
+    references_many :children, :class_name => "ControlCenter::Page", :foreign_key => :parent_id, :inverse_of => :parent
     referenced_in :parent, :class_name => "ControlCenter::Page", :inverse_of => :children
     embeds_many :grid_files, :class_name => "ControlCenter::GridFile"
 
@@ -246,45 +246,47 @@ module ControlCenter
     end
 
     def parse_raw_text
-      if self.raw_text_changed? && self.raw_text && self.raw_text.length > 0
-        self.content = {}
-        raw_text_array = self.raw_text.split("---")
-        if raw_text_array.count > 1
-          meta_data = raw_text_array.delete_at(0).strip
-          raw_text_array.each_with_index do |content, index|
-            content = content.lines.to_a
-            # Cleanup line breaks.
-            loop { content.first == "\r\n" ? content.delete_at(0) : break }
-            if content.first.include?("@ ")
-              # Extract content key from ! syntax.
-              content_key = content.delete_at(0).gsub("@ ", "").downcase
-              content_key = content_key.gsub("content", "").strip.gsub(" ", "_")
-            elsif index == 0
-              content_key = "main"
-            else
-              content_key = (index + 1).to_s
-            end
-            # Cleanup line breaks.
-            loop { content.first == "\r\n" ? content.delete_at(0) : break }
-            self.content[content_key] = content.join
-          end
-        else
-          meta_data = self.raw_text.strip
+      if self.raw_text && self.raw_text.length > 0
+        if self.new? || self.raw_text_changed?
           self.content = {}
-        end
-        meta_data = underscore_hash_keys(YAML.load(meta_data))
-        meta_data.each do |key, value|
-          unless ControlCenter::Page::PROTECTED_FIELDS.include?(key)
-            if key == :publish_time
-              self.parse_publish_time(value)
-            else
-              self[key] = value
+          raw_text_array = self.raw_text.split("---")
+          if raw_text_array.count > 1
+            meta_data = raw_text_array.delete_at(0).strip
+            raw_text_array.each_with_index do |content, index|
+              content = content.lines.to_a
+              # Cleanup line breaks.
+              loop { content.first == "\r\n" ? content.delete_at(0) : break }
+              if content.first.include?("@ ")
+                # Extract content key from ! syntax.
+                content_key = content.delete_at(0).gsub("@ ", "").downcase
+                content_key = content_key.gsub("content", "").strip.gsub(" ", "_")
+              elsif index == 0
+                content_key = "main"
+              else
+                content_key = (index + 1).to_s
+              end
+              # Cleanup line breaks.
+              loop { content.first == "\r\n" ? content.delete_at(0) : break }
+              self.content[content_key] = content.join
+            end
+          else
+            meta_data = self.raw_text.strip
+            self.content = {}
+          end
+          meta_data = underscore_hash_keys(YAML.load(meta_data))
+          meta_data.each do |key, value|
+            unless ControlCenter::Page::PROTECTED_FIELDS.include?(key)
+              if key == :publish_time
+                self.parse_publish_time(value)
+              else
+                self[key] = value
+              end
             end
           end
-        end
-        # Set the field to nil if the value isn't present in meta data.
-        (self.attributes.keys.map{ |k| k.to_sym } - PROTECTED_FIELDS).each do |field|
-          self[field] = nil if !meta_data.keys.include?(field)
+          # Set the field to nil if the value isn't present in meta data.
+          (self.attributes.keys.map{ |k| k.to_sym } - PROTECTED_FIELDS).each do |field|
+            self[field] = nil if !meta_data.keys.include?(field)
+          end
         end
       end
     end
