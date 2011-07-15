5889db0fc5bfdc06786d19749944ee0e54260d46
Steve Randy Tantra <steve.randy@gmail.com>
2011-07-15 21:57:35 +0800

    Add update_raw_text.

--- app/models/control_center/page.rb
+++ app/models/control_center/page.rb
@@ -71,7 +71,7 @@ module ControlCenter
     def content_in_html(key = "main", data={})
       if content = self.content.try(:[], key)
         content = Mustache.render(content, data)
-        content = Redcarpet.new(content, :smart, :fenced_code, :gh_blockcode).to_html
+        content = Redcarpet.new(content, :smart, :fenced_code).to_html
       else
         return nil
       end
@@ -94,7 +94,6 @@ module ControlCenter
       [:images, :stylesheets, :javascripts].each do |file_type|
         excluded_ids += self.send(file_type).map(&:_id)
       end
-      Rails.logger.info { "---#{excluded_ids}" }
       self.grid_files.where(:_id.nin => excluded_ids)
     end
 
@@ -242,55 +241,67 @@ module ControlCenter
     end
 
     def parse_raw_text
-      if self.raw_text && self.raw_text.length > 0
-        if self.new? || self.raw_text_changed?
+      if self.raw_text && self.raw_text.length > 0 && (self.new? || self.raw_text_changed?)
+        self.content = {}
+        raw_text_array = self.raw_text.split("---")
+        if raw_text_array.count > 1
+          meta_data = raw_text_array.delete_at(0).strip
+          raw_text_array.each_with_index do |content, index|
+            content = content.lines.to_a
+
+            # Cleanup line breaks.
+            loop { content.first == "\r\n" ? content.delete_at(0) : break }
+            if content.first && content.first.include?("@ ")
+              # Extract content key from @ syntax.
+              content_key = content.delete_at(0).gsub("@ ", "").downcase
+              content_key = content_key.gsub("content", "").strip.gsub(" ", "_")
+            elsif index == 0
+              content_key = "main"
+            else
+              content_key = (index + 1).to_s
+            end
+
+            # Cleanup line breaks.
+            loop { content.first == "\r\n" ? content.delete_at(0) : break }
+            self.content[content_key] = content.join
+          end
+        else
+          meta_data = self.raw_text.strip
           self.content = {}
-          raw_text_array = self.raw_text.split("---")
-          if raw_text_array.count > 1
-            meta_data = raw_text_array.delete_at(0).strip
-            raw_text_array.each_with_index do |content, index|
-              content = content.lines.to_a
+        end
 
-              # Cleanup line breaks.
-              loop { content.first == "\r\n" ? content.delete_at(0) : break }
-              if content.first && content.first.include?("@ ")
-                # Extract content key from @ syntax.
-                content_key = content.delete_at(0).gsub("@ ", "").downcase
-                content_key = content_key.gsub("content", "").strip.gsub(" ", "_")
-              elsif index == 0
-                content_key = "main"
-              else
-                content_key = (index + 1).to_s
-              end
-
-              # Cleanup line breaks.
-              loop { content.first == "\r\n" ? content.delete_at(0) : break }
-              self.content[content_key] = content.join
+        # Set each value of meta data.
+        meta_data = underscore_hash_keys(YAML.load(meta_data))
+        meta_data.each do |key, value|
+          unless ControlCenter::Page::PROTECTED_FIELDS.include?(key)
+            if key == :publish_time
+              self.parse_publish_time(value)
+            else
+              self.write_attribute(key, value)
             end
-          else
-            meta_data = self.raw_text.strip
-            self.content = {}
-          end
-
-          # Set each value of meta data.
-          meta_data = underscore_hash_keys(YAML.load(meta_data))
-          meta_data.each do |key, value|
-            unless ControlCenter::Page::PROTECTED_FIELDS.include?(key)
-              if key == :publish_time
-                self.parse_publish_time(value)
-              else
-                self.write_attribute(key, value)
-              end
-            end
-          end
-
-          # Set the field to nil if the value isn't present in meta data.
-          # Except for authors.
-          (self.attributes.keys.map{ |k| k.to_sym } - PROTECTED_FIELDS).each do |field|
-            self[field] = nil if !meta_data.keys.include?(field) && field != :authors
           end
         end
+
+        # Set the field to nil if the value isn't present in meta data.
+        # Except for authors.
+        (self.attributes.keys.map{ |k| k.to_sym } - PROTECTED_FIELDS).each do |field|
+          self[field] = nil if !meta_data.keys.include?(field) && field != :authors
+        end
+
+        self.update_raw_text
       end
+    end
+
+    def update_raw_text
+      raw_text_array = self.raw_text.split("---")
+      meta_data = raw_text_array.delete_at(0).lines.to_a
+      meta_data.each_with_index do |line, index|
+        if line.match /publish time/i
+          meta_data[index] = "Publish Time: #{self.publish_time}"
+          meta_data[index] << "\r\n" if line.include? "\r\n"
+        end
+      end
+      self.raw_text = meta_data.join + "---" + raw_text_array.join
     end
 
     def destroy_children
