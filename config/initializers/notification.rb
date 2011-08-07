5a37df7a5ca3449a146150eb71ae1951ac41f61f
Steve Randy Tantra <steve.randy@gmail.com>
2011-08-08 03:10:52 +0800

    Do not store responses for control_center controllers. Do not use safe mode.

--- config/initializers/notification.rb
+++ config/initializers/notification.rb
@@ -2,5 +2,7 @@ ActiveSupport::Notifications.subscribe "process_action.action_controller" do |na
   payload.delete(:params)
   extra_hash = {:total_runtime => (finish - start) * 1000}
   payload.merge! extra_hash
-  ControlCenter::Response.create(payload)
+  unless payload[:controller].include? "ControlCenter"
+    ControlCenter::Response.safely(false).create(payload)
+  end
 end
