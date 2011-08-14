50e21849ca21389f00c19520db43f13215415bcd
Steve Randy Tantra <steve.randy@gmail.com>
2011-08-14 14:38:19 +0800

    UI enhancements. Typekit ID is now part of engine config.

--- lib/concen.rb
+++ lib/concen.rb
@@ -1,8 +1,9 @@
 require "concen/engine" if defined?(Rails)
 
 module Concen
-  mattr_accessor :application_name
+  mattr_accessor :application_name, :typekit_id
   @@application_name = nil
+  @@typekit_id = "qxq7sbk"
 
   def self.setup
     yield self
