fc9ac414226649873cc61fb5ee217e6837dd2bd7
Steve Randy Tantra <steve.randy@gmail.com>
2011-09-27 08:54:48 +0800

    Include excanvas.js in assets precompile list.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
 Rails.application.config.assets.precompile += ["concen/ie.css", "concen/non_ios.css"]
-Rails.application.config.assets.precompile += ["concen/pages.js", "concen/performances.js", "concen/statuses.js", "concen/traffics.js", "concen/users.js"]
+Rails.application.config.assets.precompile += ["concen/pages.js", "concen/performances.js", "concen/statuses.js", "concen/traffics.js", "concen/users.js", "concen/excanvas.js"]
