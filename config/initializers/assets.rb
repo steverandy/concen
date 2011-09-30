27cef76115a82760c6a9a956a29e4b23187ab002
Steve Randy Tantra <steve.randy@gmail.com>
2011-10-01 00:39:38 +0800

    Only add additional precompile assets in production.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,4 @@
-Rails.application.config.assets.precompile += ["concen/ie.css", "concen/non_ios.css"]
-Rails.application.config.assets.precompile += ["concen/pages.js", "concen/performances.js", "concen/statuses.js", "concen/traffics.js", "concen/users.js", "concen/excanvas.js"]
+if Rails.env.production?
+  Rails.application.config.assets.precompile += %w(concen/ie.css concen/non_ios.css)
+  Rails.application.config.assets.precompile += %w(concen/pages.js concen/performances.js concen/statuses.js concen/traffics.js concen/users.js concen/excanvas.js)
+end
