baa55dd3d7c03d91ff9bb126c1db44d1f2eada15
Steve Randy Tantra <steve.randy@gmail.com>
2011-10-29 13:39:51 +0800

    Convert all views to erb.

--- app/helpers/concen/application_helper.rb
+++ app/helpers/concen/application_helper.rb
@@ -1,10 +1,4 @@
 module Concen
   module ApplicationHelper
-    # Remove all the new lines from the output.
-    # This is very useful when used for inline-block elements, because
-    # white spaces will transform into extra gaps between element.
-    def one_line(&block)
-      (capture_haml(&block).gsub("\n", '')).html_safe
-    end
   end
 end
